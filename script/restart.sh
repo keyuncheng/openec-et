ebafe3294b358bd1b459fc8433f1482df46a4433
Keyun Cheng <keyun_cheng@qq.com>
2023-01-06 11:46:53 +0800

    [add] remove unnecessary files

--- script/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-oec_packetsize=$1
-
-bash ./stop.sh
-bash ./start.sh ${oec_packetsize}
--- script/updateIP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-my_ip=$(ifconfig | grep 192.168.10 | head -1 | sed "s/ *inet [addr:]*\([^ ]*\).*/\1/")
-
-sed -i "s%\(<attribute><name>local.addr</name><value>\)[^<]*%\1${my_ip}%" conf/sysSetting.xml
-sed -i "s%\(<property><name>oec.local.addr</name><value>\)[^<]*%\1${my_ip}%" /home/openec/hadoop-3.0.0-src/hadoop-dist/target/hadoop-3.0.0/etc/hadoop/hdfs-site.xml
